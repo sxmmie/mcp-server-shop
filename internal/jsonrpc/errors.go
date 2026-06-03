a1211df1f6375ed87e4ffa3347c65771c04c1f14
Sxmmie <samuelumohjnr@gmail.com>
2026-06-03 17:24:18 +0100

    Added error helper functions

--- internal/jsonrpc/errors.go
+++ internal/jsonrpc/errors.go
@@ -7,3 +7,31 @@ const (
 	ErrorInvalidParams  = -32602 // Invalid method parameters
 	ErrorInternal       = -32603 // Internal JSON-RPC error
 )
+
+// NewError creates a new JSON-RPC error
+func NewError(code int, message string, data any) *Error {
+	return &Error{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func NewParseError(data any) *Error {
+	return NewError(ErrorParseError, "Parse error", data)
+}
+
+// creates a method not found error
+func NewMethodNotFoundError(method string) *Error {
+	return NewError(ErrorMethodNotFound, "method not found", nil)
+}
+
+// creates an invalid params error
+func NewInValidParameterError(message string) *Error {
+	return NewError(ErrorInvalidParams, message, nil)
+}
+
+// creates an internal error
+func NewInternalError(err error) *Error {
+	return NewError(ErrorInvalidParams, "internal error", err.Error())
+}
